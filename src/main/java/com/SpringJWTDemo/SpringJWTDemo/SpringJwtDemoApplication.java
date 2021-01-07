cb2352be87f9ca9d6faca8fb24061c3ff4ee8885
Akshata <akshatarjadhav14@gmail.com>
2021-01-07 17:51:13 +0530

    Initial commit

---
+++ src/main/java/com/SpringJWTDemo/SpringJWTDemo/SpringJwtDemoApplication.java
@@ -0,0 +1,15 @@
+package com.SpringJWTDemo.SpringJWTDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages="com.SpringJWTDemo.SpringJWTDemo")
+public class SpringJwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtDemoApplication.class, args);
+	}
+
+}
